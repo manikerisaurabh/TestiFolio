106ad57a076968ce0d531b35da4fee0b1bbcb82f
manikerisaurabh <manikerisaurabh@gmail.com>
2024-11-29 17:45:02 +0530

    fixing the db connection in prod

--- src/lib/connetToDb.ts
+++ src/lib/connetToDb.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectToDb = async () => {
     try {
         console.log('in connectToDb function');
-        console.log('this is mongo url ', process.env.MONGO_DB_URL)
-        if (process.env.MONGO_DB_URL) {
-            await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log('this is mongo url ', process.env.NEXT_PUBLIC_MONGO_DB_URL)
+        if (process.env.NEXT_PUBLIC_MONGO_DB_URL) {
+            await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URL);
             console.log("Connected to mongodb");
 
         }
